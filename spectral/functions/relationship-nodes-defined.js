37cab24b1c0fa6ed529d5f8e4b51b0c06000ef15
Will Osborne <will.osborne@morganstanley.com>
2024-02-22 16:55:20 +0000

    Add spectral rulesets and readme

---
+++ spectral/functions/relationship-nodes-defined.js
@@ -0,0 +1,16 @@
+export default (input, _, context) => {
+    if (!input) {
+        return [];
+    }
+    // get uniqueIds of all nodes
+    let names = context.document.data.nodes.map(node => node.uniqueId);
+    let results = [];
+
+    if (!names.includes(input)) {
+        results.push({
+            message: `'${input}' does not refer to the uniqueId of an existing node.`,
+            path: [...context.path]
+        });
+    }
+    return results;
+}
