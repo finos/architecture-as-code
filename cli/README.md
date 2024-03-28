2ce8585871433f71db78533d8fa26396613e19d3
Will Osborne <will.osborne@morganstanley.com>
2024-03-28 15:32:07 +0000

    CLI: add generate command, restructure slightly and add unit testing with jest (#89)

--- cli/README.md
+++ cli/README.md
@@ -5,10 +5,15 @@ To get started, clone the project and run the following commands:
 ```
 npm install
 npm run build
-npm link
+npx link
 ```
 
 When you've made a change to the CLI and want to test it out, you can rerun the build and link steps.
-This will make the CLI available on your machine.
+This will make the CLI available on your local `node_modules` path.
 
-Type `calm` into your terminal, and you should see the help text printed out.
+Type `npx calm` into your terminal, and you should see the help text printed out.
+
+`npx link` uses the `link` package to symlink the `calm` executable in `node_modules/.bin` to your locally-built CLI.
+
+Note: you can also use `npm link` but this installs to your global package registry.
+This will make the executable available as just `calm`, but will pollute your global NPM profile and may require `sudo` depending on your OS.
---
+++ cli/src/commands/generate/generate.ts
@@ -0,0 +1,128 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { mkdirp } from 'mkdirp';
+
+import * as winston from 'winston';
+
+let logger: winston.Logger; // defined later at startup
+
+function loadFile(path: string): any {
+    logger.info('Loading pattern from file: ' + path);
+    const raw = fs.readFileSync(path, { encoding: 'utf8' });
+
+    logger.debug('Attempting to load pattern file: ' + raw);
+    const pattern = JSON.parse(raw);
+
+    logger.debug('Loaded pattern file.');
+    return pattern;
+}
+
+
+function getStringPlaceholder(name: string): string {
+    return '{{ ' + name.toUpperCase().replaceAll('-', '_') + ' }}';
+}
+
+function getPropertyValue(keyName: string, detail: any) : any {
+    if ('const' in detail) {
+        return detail['const'];
+    }
+
+    if ('type' in detail) {
+        const propertyType = detail['type'];
+
+        if (propertyType === 'string') {
+            return getStringPlaceholder(keyName);
+        }
+        if (propertyType === 'integer') {
+            return -1;
+        }
+    }
+}
+
+function instantiateNodes(pattern: any): any {
+    const nodes = pattern?.properties?.nodes?.prefixItems;
+    if (!nodes) {
+        console.error('Warning: pattern has no nodes defined.');
+        return [];
+    }
+    const outputNodes = [];
+
+    for (const node of nodes) {
+        if (!('properties' in node)) {
+            continue;
+        }
+
+        const out = {};
+        for (const [key, detail] of Object.entries(node['properties'])) {
+            out[key] = getPropertyValue(key, detail);
+        }
+
+        outputNodes.push(out);
+    }
+    return outputNodes;
+}
+
+function getRelationships(pattern: any): any {
+    const relationships = pattern?.properties?.relationships?.prefixItems;
+
+    if (!relationships) {
+        console.error('Warning: pattern has no relationships defined');
+        return [];
+    }
+
+    const outputRelationships = [];
+    for (const relationship of relationships) {
+        if (!('properties' in relationship)) {
+            continue;
+        }
+
+        const out = {};
+        for (const [key, detail] of Object.entries(relationship['properties'])) {
+            if (key === 'relationship-type') {
+                out[key] = getPropertyValue(key, detail);
+            }
+            else {
+                out[key] = getPropertyValue(key, detail);
+            }
+        }
+
+        outputRelationships.push(out);
+    }
+
+    return outputRelationships;
+}
+
+export const exportedForTesting = {
+    getPropertyValue
+};
+
+export function runGenerate (patternPath: string, outputPath: string, debug: boolean): void {
+    const level = debug ? 'debug' : 'info';
+    logger = winston.createLogger({
+        transports: [
+            new winston.transports.Console()
+        ],
+        level: level,
+        format: winston.format.cli()
+    });
+
+
+    const pattern = loadFile(patternPath);
+    const outputNodes = instantiateNodes(pattern);
+    const relationshipNodes = getRelationships(pattern);
+
+
+    const final = {
+        'nodes': outputNodes,
+        'relationships': relationshipNodes
+    };
+    const output = JSON.stringify(final, null, 2);
+    logger.debug('Generated instantiation: ' + output);
+
+    const dirname = path.dirname(outputPath);
+
+    mkdirp.sync(dirname);
+    fs.writeFileSync(outputPath, output);
+}
