920a05e993d7a7da7eec1f80b624a64c3ade3adc
Luigi Bulanti <bulantiluigi@gmail.com>
2024-04-10 09:23:28 +0100

    issue-73 - fixing imports for visualize command

--- cli/src/commands/visualize/visualize.ts
+++ cli/src/commands/visualize/visualize.ts
@@ -1,10 +1,10 @@
 import { visualize } from '@finos/calm-visualizer';
 import * as fs from 'node:fs';
 import * as winston from 'winston';
-import { initLogger } from '../helper';
+import { initLogger } from '../helper.js';
 
 export default async function(input: string, output: string, debug: boolean) {
-    const logger = initLogger(debug);
+    const logger: winston.Logger = initLogger(debug);
 
     logger.info(`Reading CALM file from [${input}]`);
     const calm = fs.readFileSync(input, 'utf-8');
