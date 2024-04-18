54403acc759d2833a42bd6ac0f159d84b66f7e60
Will Osborne <will.osborne@morganstanley.com>
2024-04-18 11:00:15 +0100

    Fix relationship reference rules

--- spectral/pattern/functions/node-id-exists.js
+++ spectral/pattern/functions/node-id-exists.js
@@ -1,3 +1,4 @@
+import { JSONPath } from 'jsonpath-plus'
 /**
  * Checks that the input value exists as a node with a matching unique ID.
  */
@@ -5,8 +6,10 @@ export default (input, _, context) => {
     if (!input) {
         return [];
     }
+
+    const names = JSONPath({path: '$.properties.nodes.prefixItems[*].properties.unique-id.const', json: context.document.data});
+
     // get uniqueIds of all nodes
-    let names = context.document.data.nodes.map(node => node["unique-id"]);
     let results = [];
 
     if (!names.includes(input)) {
