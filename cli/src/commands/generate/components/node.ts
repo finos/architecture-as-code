a252530895f52ab26d75eff5d059734aa075c774
Will Osborne <will.osborne@morganstanley.com>
2024-05-03 09:51:05 +0100

    remove log line

--- cli/src/commands/generate/components/node.ts
+++ cli/src/commands/generate/components/node.ts
@@ -115,7 +115,6 @@ export function instantiateNodeInterfaces(detail: any, schemaDirectory: SchemaDi
     const logger = initLogger(debug);
     const interfaces = [];
     if (!('prefixItems' in detail)) {
-        logger.debug('Instantiating a node with no interfaces defined.');
         return [];
     }
 
