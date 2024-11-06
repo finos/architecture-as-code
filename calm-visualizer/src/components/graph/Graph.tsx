6faf68f9c83d356febb65cab463179dd7a0aa6a0
Adwoa Konadu Appiah <konadu.appiah@turntabl.io>
2024-11-06 13:06:09 +0000

    Added group and nested group functionality

--- calm-visualizer/src/components/graph/Graph.tsx
+++ calm-visualizer/src/components/graph/Graph.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import './Graph.css'
 import { BrowserJsPlumbInstance } from "@jsplumb/browser-ui"
 import { NodeLayout, RelationshipLayout } from "../../layout";
-import { CALMConnectsRelationship, CALMInteractsRelationship, CALMRelationship } from "../../types";
+import { CALMComposedOfRelationship, CALMConnectsRelationship, CALMDeployedInRelationship, CALMInteractsRelationship, CALMRelationship } from "../../types";
 
 interface Props {
     instance: BrowserJsPlumbInstance,
@@ -12,84 +12,128 @@ interface Props {
 
 // const groups: string[] = [];
 
-function createConnectsRelationship(instance: BrowserJsPlumbInstance, relationship: CALMConnectsRelationship) {
-    const r = relationship["relationship-type"]["connects"];
-    instance.connect({
-        source: document.getElementById(r.source.node)!,
-        target: document.getElementById(r.destination.node)!,
-        anchor: "Continuous",
-        connector: {
-            type:"Straight",
-            options: {
-                "stub": 25
+function createConnectsRelationship(instance: BrowserJsPlumbInstance, relationships: CALMConnectsRelationship[]) {
+    relationships.map(
+            relationship =>{
+                const connects  = relationship["relationship-type"]["connects"];
+                instance.connect({
+                    source: document.getElementById(connects.source.node)!,
+                    target: document.getElementById(connects.destination.node)!,
+                    anchor: "Continuous",
+                    connector: {
+                        type:"Straight",
+                        options: {
+                            "stub": 25
+                        }
+                    },
+                    endpoint: {
+                        type: "Blank",
+                        options: {}
+                    },
+                    overlays: [
+                        { type:"Arrow", options:{location:1}}
+                    ]
+                });
             }
-        },
-        endpoint: {
-            type: "Blank",
-            options: {}
-        },
-        overlays: [
-            { type:"Arrow", options:{location:1}}
-        ]
-    });
+        );
+    }
+
+    function createInteractsRelationship(instance: BrowserJsPlumbInstance, relationships: CALMInteractsRelationship[]) {
+        relationships.map(relationship => {
+            const interacts = relationship["relationship-type"]["interacts"];
+            interacts.nodes.forEach(node => {
+                instance.connect({
+                    source: document.getElementById(interacts.actor)!,
+                    target: document.getElementById(node)!,
+                    anchor: "Continuous",
+                    connector: {
+                        type:"Straight",
+                        options: {
+                            "stub": 25
+                        }
+                    },
+                    endpoint: {
+                        type: "Blank",
+                        options: {
+                        }
+                    },
+                    overlays:[ 
+                        { type:"Arrow", options:{location:1}}
+                    ]
+                });
+            })
+        })
+    }
+
+function getUniqueCALMDeployedInContainers(relationships : CALMDeployedInRelationship[]){
+    const deployedInContainers : string[] =  relationships.map(relationship=> relationship["relationship-type"]["deployed-in"].container);
+    return Array.from(new Set(deployedInContainers));
 }
 
-function createInteractsRelationship(instance: BrowserJsPlumbInstance, relationship: CALMInteractsRelationship) {
-    const r = relationship["relationship-type"]["interacts"];
-    r.nodes.forEach(node => {
-        instance.connect({
-            source: document.getElementById(r.actor)!,
-            target: document.getElementById(node)!,
-            anchor: "Continuous",
-            connector: {
-                type:"Straight",
-                options: {
-                    "stub": 25
-                }
-            },
-            endpoint: {
-                type: "Blank",
-                options: {
-                }
-            },
-            overlays: [
-                { type:"Arrow", options:{location:1}}
-            ]
-        });
+function createGroups({instance, containers}:{instance: BrowserJsPlumbInstance, containers: string[]}){
+    containers.forEach(container =>{
+        instance.addGroup({
+            el: document.getElementById(container)!,
+            id: container,
+            anchor:"Continuous",
+            endpoint:{ type:"Dot", options:{ radius:3 } },
+            dropOverride:true
+        })   
+    })
+
+}
+
+function createDeployedInRelationship( instance: BrowserJsPlumbInstance , relationships: CALMDeployedInRelationship[] ) {
+    const containers = getUniqueCALMDeployedInContainers(relationships)
+    createGroups({instance, containers}); 
+    relationships.map(relationship => {
+        const container = relationship["relationship-type"]["deployed-in"].container
+        const nodes = relationship["relationship-type"]["deployed-in"].nodes
+        if ( instance.getGroup(container).id === container){
+            nodes.forEach(node => {
+                instance.addToGroup(container, document.getElementById(node)!)
+            })
+        }
     })
 }
 
-// function createDeployedInRelationship(instance: BrowserJsPlumbInstance, relationship: CALMDeployedInRelationship) {
-//     const r = relationship["relationship-type"]["deployed-in"];
-//     if (!groups.find(group => group === r.container)) {
-//         groups.push(r.container);
-//         instance.addGroup({
-//             el: document.getElementById(r.container)!,
-//             id: r.container,
-//         });
-//     }
+function getUniqueCALMComposedOfContainers(relationships : CALMComposedOfRelationship[]){
+    const composedOfContainers : string[] =  relationships.map(relationship=> relationship["relationship-type"]["composed-of"].container);
+    return Array.from(new Set(composedOfContainers));
+}
 
-//     r.nodes.forEach(node => {
-//         instance.addToGroup(r.container, document.getElementById(node)!)
-//     });
-// }
+function createComposedOfRelationship( instance: BrowserJsPlumbInstance , relationships: CALMComposedOfRelationship[] ) {
+    const containers =  getUniqueCALMComposedOfContainers(relationships)
+    createGroups({instance, containers}); 
+    relationships.map(relationship => {
+        const container = relationship["relationship-type"]["composed-of"].container
+        const nodes = relationship["relationship-type"]["composed-of"].nodes
+        if ( instance.getGroup(container).id === container){
+            nodes.forEach(node => {
+                instance.addToGroup(container, document.getElementById(node)!)
+            })
+        }
+    })
+}
 
-function createGraphRelationship(instance: BrowserJsPlumbInstance, relationship: CALMRelationship) {
-    if ("connects" in relationship["relationship-type"]) {
-        createConnectsRelationship(instance, relationship as CALMConnectsRelationship);
-    } else if ("interacts" in relationship["relationship-type"]) {
-        createInteractsRelationship(instance, relationship as CALMInteractsRelationship);
-    } else if ("deployed-in" in relationship["relationship-type"]) {
-        // createDeployedInRelationship(instance, relationship as CALMDeployedInRelationship);
-        // TODO: Fix the "deployed-in" relationship and add "contains" relationship
-    }
+function createGraphRelationship(instance: BrowserJsPlumbInstance, relationships: CALMRelationship[]) {
+    const filterDeployedIn : CALMDeployedInRelationship[] = relationships.filter(relationship => 'deployed-in' in relationship["relationship-type"]).map(relationship => relationship as CALMDeployedInRelationship);
+    const filterComposedOf = relationships.filter(relationship => 'composed-of' in relationship["relationship-type"]).map(relationship => relationship as CALMComposedOfRelationship);
+    const filterConnect = relationships.filter(relationship => 'connects' in relationship["relationship-type"]).map(relationship => relationship as CALMConnectsRelationship)
+    const filterInteracts = relationships.filter(relationship => 'interacts' in relationship["relationship-type"]).map(relationship => relationship as CALMInteractsRelationship)
+
+    createConnectsRelationship(instance, filterConnect);
+
+    createInteractsRelationship(instance, filterInteracts);
+
+    createDeployedInRelationship(instance, filterDeployedIn);
+
+    createComposedOfRelationship(instance, filterComposedOf); 
 }
 
 function Graph({ instance, nodes, relationships }: Props) {
     useEffect(() => {
-        relationships.map(relationship => {
-            createGraphRelationship(instance, relationship);
-        });
+            createGraphRelationship(instance, relationships);
 
         nodes.forEach(node => {
             instance.manage(document.getElementById(node["unique-id"])!);
