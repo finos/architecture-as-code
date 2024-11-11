cfaaff79e730540101ee5723fa822451ae985530
oliviajanejohns <olivia.johnson@morganstanley.com>
2024-11-11 19:16:27 +0000

    Add arrow labels on graph outputted by calm-visualizer (finos#568)

--- calm-visualizer/src/components/graph/Graph.tsx
+++ calm-visualizer/src/components/graph/Graph.tsx
@@ -27,8 +27,8 @@ function createConnectsRelationship(instance: BrowserJsPlumbInstance, relationsh
             options: {}
         },
         overlays: [
-            { type:"Arrow", options:{location:1}},
-            {  type: "Label", options: { location: 0.5, label: relationship.description }}
+            { type: "Arrow", options: { location: 1 }},
+            { type: "Label", options: { location: [ 0.5, 0.5 ], label:relationship.description, cssClass: 'graph-labels' }}
         ]
     });
 }
@@ -52,8 +52,8 @@ function createInteractsRelationship(instance: BrowserJsPlumbInstance, relations
                 }
             },
             overlays: [
-                { type:"Arrow", options:{location:1}},
-                {  type: "Label", options: { location: 0.5, label: relationship.description }}
+                { type: "Arrow", options: { location: 1 }},
+                { type: "Label", options: { location: [ 0.5, 0.5 ], label:relationship.description, cssClass: 'graph-labels' }}
 
             ]
         });
