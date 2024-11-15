2483dbd85eb66fb5597f362743bea3f1392dd1d0
oliviajanejohns <olivia.johnson@morganstanley.com>
2024-11-15 23:58:58 +0000

    Open node-detail-preview on node selection

--- calm-visualizer/src/App.tsx
+++ calm-visualizer/src/App.tsx
@@ -1,89 +1,12 @@
-import { useEffect, useState } from "react";
 import './App.css'
-import { BrowserJsPlumbInstance, newInstance } from "@jsplumb/browser-ui"
-import FileUploader from "./components/fileuploader/FileUploader";
-import { NodeLayout, RelationshipLayout } from "./layout";
-import Graph from "./components/graph/Graph";
-import { saveAs } from 'file-saver';
-import { CALMNode } from "./types";
-
-interface LayoutObject {
-    'unique-id': string, 
-    x: number, 
-    y: number
-}
+import Drawer from './components/drawer/Drawer'
 
 function App() {
-    const [nodes, setNodes] = useState<NodeLayout[]>([]);
-    const [relationships, setRelationships] = useState<RelationshipLayout[]>([]);
-    const [instance, setInstance] = useState<BrowserJsPlumbInstance>();
-    const [title, setTitle] = useState('Architecture as Code');
-
-    function enhanceNodesWithLayout(nodes: CALMNode[], coords: LayoutObject[]): NodeLayout[] {
-        return nodes.map(node => {
-            const coordObject = coords.find((coord) => coord["unique-id"] == node["unique-id"]);
-            return {
-                ...node,
-                x: coordObject?.x || 400,
-                y: coordObject?.y || 400
-            }
-        });
-    }
-
-    async function handleFile(instanceFile: File, layoutFile?: File) {
-        const instanceString = await instanceFile.text();
-        const calmInstance = JSON.parse(instanceString);
-        setTitle(instanceFile.name);
-
-        const nodePositions: LayoutObject[] = [];
-        if (layoutFile) {
-            const layoutString = await layoutFile.text();
-            const layout = JSON.parse(layoutString);
-            layout.nodes.forEach((node: LayoutObject) => nodePositions.push(node));
-        }
-
-        setNodes(enhanceNodesWithLayout(calmInstance.nodes, nodePositions));
-        setRelationships(calmInstance.relationships);
-    }
-
-    function onSave() {
-        const outputNodes = nodes.map(node => {
-            const bounds = document.getElementById(node["unique-id"])?.getBoundingClientRect();
-
-            return {
-                "unique-id": node["unique-id"],
-                "x": bounds?.x,
-                "y": bounds?.y
-            }
-        });   
-
-        const output = JSON.stringify({
-            nodes: outputNodes,
-            relationships
-        });     
-
-        const blob = new Blob([output], {type: "text/plain;charset=utf-8"});
-        saveAs(blob, "layout.json");
-    }
-
-    useEffect(() => {
-        setInstance(newInstance({
-            container: document.getElementById('app')!
-        }));
-    }, []);
-
     return (
         <>
-            <h1>{title}</h1>
-            
-            <FileUploader callback={handleFile}/>
-        
-            <button onClick={onSave}>SaveAs</button>
-            <div id="app">
-                {instance && <Graph instance={instance} nodes={nodes} relationships={relationships} />}
-            </div>
+            <Drawer />
         </>
-    );
+    )
 }
 
 export default App
--- calm-visualizer/src/components/graph/Graph.tsx
+++ calm-visualizer/src/components/graph/Graph.tsx
@@ -1,62 +1,78 @@
-import { useEffect } from "react";
+import { useEffect } from 'react'
 import './Graph.css'
-import { BrowserJsPlumbInstance } from "@jsplumb/browser-ui"
-import { NodeLayout, RelationshipLayout } from "../../layout";
-import { CALMConnectsRelationship, CALMInteractsRelationship, CALMRelationship } from "../../types";
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui'
+import { NodeLayout, RelationshipLayout } from '../../layout'
+import {
+    CALMConnectsRelationship,
+    CALMInteractsRelationship,
+    CALMNode,
+    CALMRelationship,
+} from '../../types'
 
-interface Props {
-    instance: BrowserJsPlumbInstance,
-    nodes: NodeLayout[],
+export interface GraphProps {
+    instance: BrowserJsPlumbInstance
+    nodes: NodeLayout[]
     relationships: RelationshipLayout[]
+    setSelectedNode: any
 }
 
-function createConnectsRelationship(instance: BrowserJsPlumbInstance, relationship: CALMConnectsRelationship) {
-    const r = relationship["relationship-type"]["connects"];
+function createConnectsRelationship(
+    instance: BrowserJsPlumbInstance,
+    relationship: CALMConnectsRelationship
+) {
+    const r = relationship['relationship-type']['connects']
     instance.connect({
         source: document.getElementById(r.source.node)!,
         target: document.getElementById(r.destination.node)!,
-        anchor: "Continuous",
+        anchor: 'Continuous',
         connector: {
-            type:"Straight",
+            type: 'Straight',
             options: {
-                "stub": 25
-            }
+                stub: 25,
+            },
         },
         endpoint: {
-            type: "Blank",
-            options: {}
+            type: 'Blank',
+            options: {},
         },
         overlays: [
-            { type:"Arrow", options:{location:1}},
-            {  type: "Label", options: { location: 0.5, label: relationship.description }}
-        ]
-    });
+            { type: 'Arrow', options: { location: 1 } },
+            {
+                type: 'Label',
+                options: { location: 0.5, label: relationship.description },
+            },
+        ],
+    })
 }
 
-function createInteractsRelationship(instance: BrowserJsPlumbInstance, relationship: CALMInteractsRelationship) {
-    const r = relationship["relationship-type"]["interacts"];
-    r.nodes.forEach(node => {
+function createInteractsRelationship(
+    instance: BrowserJsPlumbInstance,
+    relationship: CALMInteractsRelationship
+) {
+    const r = relationship['relationship-type']['interacts']
+    r.nodes.forEach((node) => {
         instance.connect({
             source: document.getElementById(r.actor)!,
             target: document.getElementById(node)!,
-            anchor: "Continuous",
+            anchor: 'Continuous',
             connector: {
-                type:"Straight",
+                type: 'Straight',
                 options: {
-                    "stub": 25
-                }
+                    stub: 25,
+                },
             },
             endpoint: {
-                type: "Blank",
-                options: {
-                }
+                type: 'Blank',
+                options: {},
             },
             overlays: [
-                { type:"Arrow", options:{location:1}},
-                {  type: "Label", options: { location: 0.5, label: relationship.description }}
-
-            ]
-        });
+                { type: 'Arrow', options: { location: 1 } },
+                {
+                    type: 'Label',
+                    options: { location: 0.5, label: relationship.description },
+                },
+            ],
+        })
     })
 }
 
@@ -75,50 +91,73 @@ function createInteractsRelationship(instance: BrowserJsPlumbInstance, relations
 //     });
 // }
 
-function createGraphRelationship(instance: BrowserJsPlumbInstance, relationship: CALMRelationship) {
-    if ("connects" in relationship["relationship-type"]) {
-        createConnectsRelationship(instance, relationship as CALMConnectsRelationship);
-    } else if ("interacts" in relationship["relationship-type"]) {
-        createInteractsRelationship(instance, relationship as CALMInteractsRelationship);
-    } else if ("deployed-in" in relationship["relationship-type"]) {
+function createGraphRelationship(
+    instance: BrowserJsPlumbInstance,
+    relationship: CALMRelationship
+) {
+    if ('connects' in relationship['relationship-type']) {
+        createConnectsRelationship(
+            instance,
+            relationship as CALMConnectsRelationship
+        )
+    } else if ('interacts' in relationship['relationship-type']) {
+        createInteractsRelationship(
+            instance,
+            relationship as CALMInteractsRelationship
+        )
+    } else if ('deployed-in' in relationship['relationship-type']) {
         // createDeployedInRelationship(instance, relationship as CALMDeployedInRelationship);
         // TODO: Fix the "deployed-in" relationship and add "contains" relationship
     }
 }
 
-function Graph({ instance, nodes, relationships }: Props) {
-    useEffect(() => {
-        relationships.map(relationship => {
-            createGraphRelationship(instance, relationship);
-        });
+function Graph({
+    instance,
+    nodes,
+    relationships,
+    setSelectedNode,
+}: GraphProps) {
+    const toggleNodePreview = (node: CALMNode) => () => {
+        setSelectedNode(node)
+    }
 
-        nodes.forEach(node => {
-            instance.manage(document.getElementById(node["unique-id"])!);
-        });
-    }, [nodes, relationships, instance]);
+    useEffect(() => {
+        relationships.map((relationship) => {
+            createGraphRelationship(instance, relationship)
+        })
+
+        nodes.forEach((node) => {
+            instance.manage(document.getElementById(node['unique-id'])!)
+        })
+    }, [nodes, relationships, instance])
 
     return (
         <>
-            {
-                nodes.map(node => {
-                    return <div 
-                        key={node["unique-id"]}
-                        id={node["unique-id"]}
+            {nodes.map((node) => {
+                return (
+                    <div
+                        onClick={toggleNodePreview(node)}
+                        key={node['unique-id']}
+                        id={node['unique-id']}
                         className="node"
                         style={{
                             left: node.x,
-                            top: node.y
-                        }} 
+                            top: node.y,
+                        }}
                     >
-                        <div><b>{node.name}</b></div>
-                        <div className="node-type">[{node["node-type"]}]</div>
+                        <div>
+                            <b>{node.name}</b>
+                        </div>
+                        <div className="node-type">[{node['node-type']}]</div>
                         <br />
-                        <div className="node-description">{node.description}</div>
+                        <div className="node-description">
+                            {node.description}
+                        </div>
                     </div>
-                })
-            }
+                )
+            })}
         </>
-    );
+    )
 }
 
 export default Graph
