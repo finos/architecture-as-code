81e1361eb8bd4204e79aab933410638c5e84eefd
Will Osborne <will.osborne@morganstanley.com>
2024-11-18 11:15:22 +0000

    Correctly log stack trace when errors are logged

--- shared/src/commands/helper.ts
+++ shared/src/commands/helper.ts
@@ -7,6 +7,15 @@ export function initLogger(debug: boolean): winston.Logger {
             new winston.transports.Console()
         ],
         level: level,
-        format: winston.format.cli(),
+        format: winston.format.combine(
+            winston.format.cli(),
+            winston.format.errors({ stack: true }),
+            winston.format.printf(({ level, message, stack }) => {
+                if (stack) {
+                    return `${level}: ${message} - ${stack}`;
+                }
+                return `${level}: ${message}`;
+            }),
+        )
     });
 }
