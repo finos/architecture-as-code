70011544ad507e5740b22693ac2d819ce8d193db
Dorothy Ewuah <dorothyewuah@gmail.com>
2024-11-25 07:51:27 +0000

    show sidepanel for details on cytoscapenode on tap

--- calm-visualizer/src/components/cytoscape-renderer/CytoscapeRenderer.tsx
+++ calm-visualizer/src/components/cytoscape-renderer/CytoscapeRenderer.tsx
@@ -1,8 +1,9 @@
-import './cytoscape.css';
-import React, {useEffect, useRef, useState} from 'react';
-import cytoscape, {Core} from 'cytoscape';
-import nodeHtmlLabel from 'cytoscape-node-html-label';
-import coseBilkent from 'cytoscape-cose-bilkent';
+import './cytoscape.css'
+import { useEffect, useRef, useState } from 'react'
+import cytoscape, { Core } from 'cytoscape'
+import nodeHtmlLabel from 'cytoscape-node-html-label'
+import coseBilkent from 'cytoscape-cose-bilkent'
+import Sidebar from '../sidebar/Sidebar'
 
 nodeHtmlLabel(cytoscape)
 
@@ -10,79 +11,56 @@ cytoscape.use(coseBilkent)
 
 export type Node = {
     data: {
-        label: string;
-        id: string;
-        [idx: string]: string;
+        label: string
+        id: string
+        [idx: string]: string
     }
 }
 
 export type Edge = {
     data: {
         label: string
-        source: string;
-        target: string;
-        [idx: string]: string;
-    };
-
+        source: string
+        target: string
+        [idx: string]: string
+    }
 }
 
 interface Props {
-    nodes: Node[];
-    edges: Edge[];
+    nodes: Node[]
+    edges: Edge[]
 }
 
 const CytoscapeRenderer = ({ nodes = [], edges = [] }: Props) => {
-    const cyRef = useRef(null);
-    const [cy, setCy] = useState<Core | null>(null);
+    const cyRef = useRef<HTMLDivElement>(null)
+    const [cy, setCy] = useState<Core | null>(null)
+    const [selectedNode, setSelectedNode] = useState<Node['data'] | null>(null)
 
     useEffect(() => {
-        if(cy) {
-            cy.nodeHtmlLabel([
-                {
-                    query:  '.node',
-                    halign: 'center',
-                    valign: 'center',
-                    halignBox:  'center',
-                    valignBox: 'center',
-                    tpl: (data: Node["data"]) => {
-                        return `<div class="node element">
-  <p class="title">${data.label}</p>
-  <p class="type">[database]</p>
-  <p class="description">Database which stores account, trade and position state</p>
-</div>`
-                    }
-                }
-            ])
-        }
-    }, [cy]);
-
-    useEffect(() => {
-        // Destroy previous Cytoscape instance to avoid memory leaks
-        /*if (cyRef.current) {
-            cyRef.current.destroy();
-        }*/
-
+        // if (!cy) {
         // Initialize Cytoscape instance
-        const container = cyRef.current;
+        const container = cyRef.current
 
-        setCy(cytoscape({
+        if (!container) return
+
+        const cyInstance = cytoscape({
             container: container, // container to render
             elements: [...nodes, ...edges], // graph data
             style: [
                 {
                     selector: 'node',
                     style: {
-                        width: "200px",
-                        height: "200px",
-                        shape:  "rectangle",
+                        width: '200px',
+                        height: '200px',
+                        shape: 'rectangle',
                     },
                 },
                 {
                     selector: 'edge',
                     style: {
-                        'width': 2,
+                        width: 2,
                         'curve-style': 'bezier',
-                        'label':  'data(label)', // labels from data property
+                        label: 'data(label)', // labels from data property
                         'target-arrow-shape': 'triangle',
                     },
                 },
@@ -90,16 +68,61 @@ const CytoscapeRenderer = ({ nodes = [], edges = [] }: Props) => {
             layout: {
                 name: 'cose-bilkent', // Use grid layout for simplicity
             },
-        }));
+        })
 
-        /*return () => {
-            if (cyRef.current) {
-                cyRef.current.destroy();
-            }
-        };*/
-    }, [nodes, edges]); // Re-render on nodes or edges change
+        cyInstance.nodeHtmlLabel([
+            {
+                query: 'node',
+                halign: 'center',
+                valign: 'center',
+                halignBox: 'center',
+                valignBox: 'center',
+                tpl: (data: Node['data']) => {
+                    return `<div class="node element">
+  <p class="title">${data.label}</p>
+  <p class="type">[database]</p>
+  <p class="description">Database which stores account, trade and position state</p>
+</div>`
+                },
+            },
+        ])
 
-    return <div ref={cyRef} style={{ width: '90%', height: '1000px', backgroundColor: "white" }} />;
-};
+        cyInstance.on('tap', 'node', (e: any) => {
+            e.preventDefault()
+            const node = e.target
+            setSelectedNode(node.data()) // Update state with the clicked node's data
+        })
+        setCy(cyInstance)
 
-export default CytoscapeRenderer;
+        return () => {
+            cyInstance.destroy()
+        }
+        // } else {
+        //     cy.json({ elements: [...nodes, ...edges] })
+        //     cy.layout({ name: 'cose-bilkent' }).run()
+        // }
+    }, [nodes, edges]) // Re-render on cy, nodes or edges change
+
+    return (
+        <div className="relative flex h-screen w-full">
+            <div
+                ref={cyRef}
+                className="flex-1 bg-white"
+                style={{
+                    height: '100vh',
+                }}
+            />
+
+            {selectedNode && (
+                <div className="absolute right-0 top-0 h-full">
+                    <Sidebar
+                        selectedNode={selectedNode}
+                        closeSidebar={() => setSelectedNode(null)}
+                    />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default CytoscapeRenderer
