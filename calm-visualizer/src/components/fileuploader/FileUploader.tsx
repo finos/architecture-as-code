0b75cda55ceb53d57eecaa2f5b0ff61a6140880a
YoofiTT96 <joseph.pobee@turntabl.io>
2024-11-27 12:49:24 +0000

    WIP layouting - Clean up commits. Remove layout file requirements

--- calm-visualizer/src/components/fileuploader/FileUploader.tsx
+++ calm-visualizer/src/components/fileuploader/FileUploader.tsx
@@ -1,42 +1,52 @@
 import React, { useEffect, useState } from 'react';
 
-interface Props {
-    callback: (instanceFile: File, layoutFile?: File) => void
+interface FileUploaderProps {
+    callback: (instanceFile: File) => void
 }
 
-function FileUploader({ callback }: Props) {
-    const [instanceFile, setInstanceFile] = useState<File | null>(null);
-    const [layoutFile, setLayoutFile] = useState<File | null>(null);
-    const [filesChanged, setFilesChanged] = useState(false);
+function FileUploader({ callback }: FileUploaderProps) {
+    const [instanceFile, setInstanceFile] = useState<File | null>(null)
+    const [filesChanged, setFilesChanged] = useState(false)
 
     useEffect(() => {
-      setFilesChanged(true);
-    }, [instanceFile, layoutFile]);
+        setFilesChanged(true)
+    }, [instanceFile])
 
     const handleSubmit = () => {
         if (instanceFile && filesChanged) {
-            callback(instanceFile, layoutFile || undefined);
-            setFilesChanged(false); 
+            callback(instanceFile)
+            setFilesChanged(false)
         }
     };
 
-  return (
-    <>
-      <div className="input-group">
-        <div style={{
-            display: 'flex'
-        }}>
-            <div>
-                <p>CALM Instance:</p>
-                <input id="file" type="file" onChange={(e: React.ChangeEvent<HTMLInputElement>) => e.target.files && setInstanceFile(e.target.files[0])} />
+    return (
+        <>
+            <div className="input-group">
+                <div
+                    style={{
+                        display: 'flex',
+                    }}
+                >
+                    <div>
+                        <p>CALM Instance:</p>
+                        <input
+                            id="file"
+                            type="file"
+                            className="file-input w-full max-w-xs"
+                            onChange={(
+                                e: React.ChangeEvent<HTMLInputElement>
+                            ) =>
+                                e.target.files &&
+                                setInstanceFile(e.target.files[0])
+                            }
+                        />
+                    </div>
+                </div>
             </div>
-
             <div>
                 <p>Layout File:</p>
                 <input id="file" type="file" onChange={(e: React.ChangeEvent<HTMLInputElement>) => e.target.files && setLayoutFile(e.target.files[0])} />
             </div>
-        </div>
-      </div>
       {instanceFile && (
         <button 
           onClick={handleSubmit}
