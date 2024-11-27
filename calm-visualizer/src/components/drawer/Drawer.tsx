4674e2e08f26df6179acc3a9699c8cd1d1a16e88
YoofiTT96 <joseph.pobee@turntabl.io>
2024-11-27 12:49:24 +0000

    WIP layouting - Clean up commits. Remove layout file requirements

--- calm-visualizer/src/components/drawer/Drawer.tsx
+++ calm-visualizer/src/components/drawer/Drawer.tsx
@@ -1,25 +1,29 @@
 import Sidebar from '../sidebar/Sidebar'
-import saveAs from 'file-saver'
 import { useState } from 'react'
-import { NodeLayout, RelationshipLayout } from '../../layout'
-import {
-    CALMComposedOfRelationship,
-    CALMConnectsRelationship,
-    CALMDeployedInRelationship,
-    CALMInstantiation,
-    CALMInteractsRelationship,
-    CALMNode,
-} from '../../types'
+import {CALMInstantiation} from '../../../../shared/src';
 import FileUploader from '../fileuploader/FileUploader'
-import CytoscapeRenderer, {
-    Edge,
-    Node,
-} from '../cytoscape-renderer/CytoscapeRenderer.tsx'
+import CytoscapeRenderer, {Node} from "../cytoscape-renderer/CytoscapeRenderer.tsx";
+import {
+    CALMComposedOfRelationship, CALMConnectsRelationship,
+    CALMDeployedInRelationship,
+    CALMInteractsRelationship,
+    CALMRelationship
+} from "../../types";
 
-interface LayoutObject {
-    'unique-id': string
-    x: number
-    y: number
+function isComposedOf(relationship: CALMRelationship): relationship is CALMComposedOfRelationship {
+    return 'composed-of' in relationship['relationship-type'];
+}
+
+function isDeployedIn(relationship: CALMRelationship): relationship is CALMDeployedInRelationship {
+    return 'deployed-in' in relationship['relationship-type'];
+}
+
+function isInteracts(relationship: CALMRelationship): relationship is CALMInteractsRelationship {
+    return 'interacts' in relationship['relationship-type'];
+}
+
+function isConnects(relationship: CALMRelationship): relationship is CALMConnectsRelationship {
+    return 'connects' in relationship['relationship-type'];
 }
 
 const getComposedOfRelationships = (calmInstance: CALMInstantiation) => {
@@ -30,237 +34,134 @@ const getComposedOfRelationships = (calmInstance: CALMInstantiation) => {
         }
     } = {}
 
-    calmInstance.relationships
-        .filter(
-            (relationship) => 'composed-of' in relationship['relationship-type']
-        )
-        .map((r) => r as CALMComposedOfRelationship)
-        .map((relationship) => {
-            const rel = relationship['relationship-type']['composed-of']
-            composedOfRelationships[rel['container']] = { type: 'parent' }
-            rel['nodes'].forEach((node) => {
+    calmInstance.relationships.forEach(relationship => {
+        if (isComposedOf(relationship)) {
+            const rel = relationship["relationship-type"]["composed-of"];
+            composedOfRelationships[rel["container"]] = {type: "parent"}
+            rel["nodes"].forEach(node => {
                 composedOfRelationships[node] = {
                     type: 'child',
                     parent: rel['container'],
                 }
             })
-        })
+        }
+    })
 
-    return composedOfRelationships
+    return composedOfRelationships;
 }
-
-const getDeployedInRelationships = (calmInstance: CALMInstantiation) => {
+const getDeployedInRelationships = (calmInstance:  CALMInstantiation) => {
     const deployedInRelationships: {
         [idx: string]: {
             type: 'parent' | 'child'
             parent?: string
         }
     } = {}
-
-    calmInstance.relationships
-        .filter(
-            (relationship) => 'deployed-in' in relationship['relationship-type']
-        )
-        .map((r) => r as CALMDeployedInRelationship)
-        .map((relationship) => {
-            const rel = relationship['relationship-type']['deployed-in']
-            console.log(rel)
-            deployedInRelationships[rel['container']] = { type: 'parent' }
-            rel['nodes'].forEach((node) => {
+    calmInstance.relationships.forEach(relationship => {
+        if (isDeployedIn(relationship)) {
+            const rel = relationship["relationship-type"]["deployed-in"];
+            deployedInRelationships[rel["container"]] = {type: "parent"}
+            rel["nodes"].forEach(node => {
                 deployedInRelationships[node] = {
                     type: 'child',
                     parent: rel['container'],
                 }
             })
-        })
+        }
+    })
 
-    return deployedInRelationships
+    return deployedInRelationships;
 }
 
 function Drawer() {
-    const [nodes, setNodes] = useState<NodeLayout[]>([])
-    const [relationships, setRelationships] = useState<RelationshipLayout[]>([])
     const [title, setTitle] = useState('Architecture as Code')
     const [selectedNode, setSelectedNode] = useState(null)
-    const [calmInstance, setCALMInstance] = useState(null)
-    const [cyNodes, setCyNodes] = useState<Node[] | undefined>()
-    const [cyEdges, setCyEdges] = useState<Edge[] | undefined>()
+    const [calmInstance, setCALMInstance] = useState<CALMInstantiation | null>(null)
 
     const closeSidebar = () => {
         setSelectedNode(null)
     }
 
-    function enhanceNodesWithLayout(
-        nodes: CALMNode[],
-        coords: LayoutObject[]
-    ): NodeLayout[] {
-        return nodes.map((node) => {
-            const coordObject = coords.find(
-                (coord) => coord['unique-id'] == node['unique-id']
-            )
-            return {
-                ...node,
-                x: coordObject?.x || 400,
-                y: coordObject?.y || 400,
-            }
-        })
-    }
+    const getNodesAndEdges = () => {
+        if(!calmInstance) return [[], []];
+        const composedOfRelationships = getComposedOfRelationships(calmInstance);
+        const deployedInRelationships = getDeployedInRelationships(calmInstance);
 
-    const getData = (calmInstance: CALMInstantiation) => {
-        const composedOfRelationships = getComposedOfRelationships(calmInstance)
-        const deployedInRelationships = getDeployedInRelationships(calmInstance)
-
-        const nodes = calmInstance.nodes.map((node) => {
+        const nodes =  calmInstance ? calmInstance.nodes.map(node => {
             const newData: Node = {
                 classes: 'node',
                 data: {
-                    classes: 'node',
                     label: node.name,
-                    description: node.description,
-                    type: node['node-type'],
-                    id: node['unique-id'],
-                },
+                    description: node.description, type: node["node-type"], id: node["unique-id"]
+                }
             }
 
-            if (composedOfRelationships[node['unique-id']]?.type === 'parent') {
-                newData.classes = 'group'
+            if (composedOfRelationships[node["unique-id"]]?.type === "parent") {
+                newData.classes = "group";
             }
 
-            if (
-                composedOfRelationships[node['unique-id']]?.type === 'child' &&
-                composedOfRelationships[node['unique-id']]['parent']
-            ) {
-                newData.data.parent =
-                    composedOfRelationships[node['unique-id']].parent!
+            if (composedOfRelationships[node["unique-id"]]?.type === "child" && composedOfRelationships[node["unique-id"]]["parent"]) {
+                newData.data.parent = composedOfRelationships[node["unique-id"]].parent!;
             }
 
-            if (deployedInRelationships[node['unique-id']]?.type === 'parent') {
-                newData.classes = 'group'
+            if (deployedInRelationships[node["unique-id"]]?.type === "parent") {
+                newData.classes = "group";
             }
 
-            if (
-                deployedInRelationships[node['unique-id']]?.type === 'child' &&
-                deployedInRelationships[node['unique-id']]['parent'] &&
-                !newData.data.parent
-            ) {
-                newData.data.parent =
-                    deployedInRelationships[node['unique-id']].parent!
+            if (deployedInRelationships[node["unique-id"]]?.type === "child" && deployedInRelationships[node["unique-id"]]["parent"] && !newData.data.parent) {
+                newData.data.parent = deployedInRelationships[node["unique-id"]].parent!;
             }
-            return newData
-        })
+            return newData;
+        }): []
 
-        const connectsRelationships = calmInstance.relationships
-            .filter(
-                (relationship) =>
-                    'connects' in relationship['relationship-type']
-            )
-            .map((r) => r as CALMConnectsRelationship)
-            .map((relationship) => {
-                if (
-                    'connects' in relationship['relationship-type'] &&
-                    relationship['unique-id'] &&
-                    relationship.description &&
-                    relationship['relationship-type'].connects.source.node &&
-                    relationship['relationship-type'].connects.destination.node
-                ) {
-                    const source =
-                        relationship['relationship-type'].connects.source.node
-                    const target =
-                        relationship['relationship-type'].connects.destination
-                            .node
-                    return {
-                        data: {
-                            id: relationship['unique-id'],
-                            label: relationship.description,
-                            source,
-                            target,
-                            smooth: {
-                                enabled: true,
-                                type: 'curvedCW',
-                                roundness: 0.1,
-                            },
-                        },
+        const relationships = calmInstance ? calmInstance.relationships.filter(relationship =>  !isComposedOf(relationship) && !isDeployedIn(relationship)).map(relationship => {
+            if (isInteracts(relationship) && relationship["unique-id"] && relationship.description) {
+                return {
+                    data: {
+                        id: relationship["unique-id"],
+                        label: relationship.description,
+                        source: relationship["relationship-type"].interacts.actor,
+                        target: relationship["relationship-type"].interacts.nodes[0],
+                        smooth: {
+                            enabled: true,
+                            type: "curvedCW",
+                            roundness: 0.1
+                        }
                     }
                 }
-            })
-            .filter((relationship) => relationship != undefined)
-
-        const interactsRelationship = calmInstance.relationships
-            .filter(
-                (relationship) =>
-                    'interacts' in relationship['relationship-type']
-            )
-            .map((r) => r as CALMInteractsRelationship)
-            .map((relationship) => {
-                if (
-                    'interacts' in relationship['relationship-type'] &&
-                    relationship['unique-id'] &&
-                    relationship.description
-                ) {
-                    return {
-                        data: {
-                            id: relationship['unique-id'],
-                            label: relationship.description,
-                            source: relationship['relationship-type'].interacts
-                                .actor,
-                            target: relationship['relationship-type'].interacts
-                                .nodes[0],
-                            smooth: {
-                                enabled: true,
-                                type: 'curvedCW',
-                                roundness: 0.1,
-                            },
-                        },
+            }
+            if (isConnects(relationship) && relationship["unique-id"] && relationship.description && relationship["relationship-type"].connects.source.node && relationship["relationship-type"].connects.destination.node) {
+                const source = relationship["relationship-type"].connects.source.node;
+                const target = relationship["relationship-type"].connects.destination.node;
+                return {
+                    data: {
+                        id: relationship["unique-id"],
+                        label: relationship.description,
+                        source,
+                        target,
+                        smooth: {
+                            enabled: true,
+                            type: "curvedCW",
+                            roundness: 0.1
+                        }
                     }
                 }
-            })
-            .filter((relationship) => relationship != undefined)
+            }
 
-        setCyNodes(nodes)
-        setCyEdges([...interactsRelationship, ...connectsRelationships])
+        }) : []
+
+        return [nodes, relationships]
     }
 
-    async function handleFile(instanceFile: File, layoutFile?: File) {
+    const [nodes,  relationships] = getNodesAndEdges();
+
+
+    async function handleFile(instanceFile: File) {
         const instanceString = await instanceFile.text()
-        const calmInstance = JSON.parse(instanceString)
-        setTitle(instanceFile.name)
-
-        const nodePositions: LayoutObject[] = []
-        if (layoutFile) {
-            const layoutString = await layoutFile.text()
-            const layout = JSON.parse(layoutString)
-            layout.nodes.forEach((node: LayoutObject) =>
-                nodePositions.push(node)
-            )
-        }
-
-        setCALMInstance(calmInstance)
-        getData(calmInstance)
-        setNodes(enhanceNodesWithLayout(calmInstance.nodes, nodePositions))
-        setRelationships(calmInstance.relationships)
+        const calmInstance:CALMInstantiation = JSON.parse(instanceString);
+        setTitle(instanceFile.name);
+        setCALMInstance(calmInstance);
     }
 
-    function onSave() {
-        const outputNodes = nodes.map((node) => {
-            const bounds = document
-                .getElementById(node['unique-id'])
-                ?.getBoundingClientRect()
-
-            return {
-                'unique-id': node['unique-id'],
-                x: bounds?.x,
-                y: bounds?.y,
-            }
-        })
-
-        const output = JSON.stringify({
-            nodes: outputNodes,
-            relationships,
-        })
-
-        const blob = new Blob([output], { type: 'text/plain;charset=utf-8' })
-        saveAs(blob, 'layout.json')
-    }
 
     return (
         <>
@@ -276,16 +177,8 @@ function Drawer() {
                 <div className="drawer-content">
                     <div className="text-xl font-bold">{title}</div>
                     <FileUploader callback={handleFile} />
-                    <button className="btn" onClick={onSave}>
-                        SaveAs
-                    </button>
                     <div id="app">
-                        {calmInstance && (
-                            <CytoscapeRenderer
-                                nodes={cyNodes}
-                                edges={cyEdges}
-                            />
-                        )}
+                        {calmInstance && <CytoscapeRenderer nodes={nodes} edges={relationships}/>}
                     </div>
                 </div>
                 {selectedNode && (
