50c7b9bda61a12ff1b0c7833507bb0f806913272
Thels <ross.thels@gmail.com>
2024-11-28 13:05:41 +0000

    This fixes #316 (#634)
    Introducing a `-pretty` flag to the validate command.

--- cli/jest.config.js
+++ cli/jest.config.js
@@ -3,9 +3,12 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/*.spec.ts'],
     transformIgnorePatterns: [
-        "<rootDir>/node_modules/(?!@finos/calm-shared)",
+        '<rootDir>/node_modules/(?!@finos/calm-shared)',
         '^.+\\.js$'
     ],
+    transform: {
+        '^.+\\.ts?$': 'ts-jest', 
+    },
     rootDir: '.',
     watchPathIgnorePatterns: ['<rootDir>/../shared/']
 };
