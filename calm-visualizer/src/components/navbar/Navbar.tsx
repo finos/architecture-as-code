5e5000b343518d4d9aa14f460f3dcf03e5066cbc
oliviajanejohns <olivia.johnson@morganstanley.com>
2024-12-20 22:21:10 +0000

    Added Toggle to show/hide descriptions on nodes

--- calm-visualizer/src/components/navbar/Navbar.tsx
+++ calm-visualizer/src/components/navbar/Navbar.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { CALMInstantiation } from '../../../../shared/src';
 
 interface NavbarProps {
     handleUpload: (instanceFile: File) => void;
-    toggleDescriptions: () => void;
-    calmInstance?: CALMInstantiation;
+    isGraphRendered: boolean;
+    toggleConnectionDesc: () => void;
+    toggleNodeDesc: () => void;
 }
 
-function Navbar({ handleUpload, toggleDescriptions, calmInstance }: NavbarProps) {
+function Navbar({
+    handleUpload,
+    isGraphRendered,
+    toggleConnectionDesc,
+    toggleNodeDesc,
+}: NavbarProps) {
     const upload = (file: File) => {
         handleUpload(file);
     };
@@ -16,19 +21,22 @@ function Navbar({ handleUpload, toggleDescriptions, calmInstance }: NavbarProps)
         <div className="navbar bg-base-300">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">CALM</a>
+                <div className="divider divider-horizontal"></div>
+                <span className="text-lg">Visualizer</span>
             </div>
             <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1" aria-label="navbar-menu-items">
                     <li>
                         <details>
                             <summary>Upload</summary>
-                            <ul className="p-2 z-1">
+                            <ul className="p-2 z-1" aria-label="upload-dropdown-items">
                                 <li>
                                     <label>
                                         Architecture
                                         <input
                                             id="file"
                                             type="file"
+                                            aria-label="upload-architecture"
                                             className="hidden"
                                             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                                                 e.target.files && upload(e.target.files[0])
@@ -40,18 +48,33 @@ function Navbar({ handleUpload, toggleDescriptions, calmInstance }: NavbarProps)
                         </details>
                     </li>
                 </ul>
-                <div className="toggles">
-                    {calmInstance && (
-                        <label className="label cursor-pointer">
-                            <span className="label-text">Connection Descriptions</span>
-                            <input
-                                type="checkbox"
-                                className="toggle"
-                                onClick={toggleDescriptions}
-                            />
-                        </label>
-                    )}
-                </div>
+
+                {isGraphRendered && (
+                    <>
+                        <div className="divider divider-horizontal"></div>
+                        <div className="toggles menu-horizontal">
+                            <label className="label cursor-pointer">
+                                <span className="label label-text">Connection Descriptions</span>
+                                <input
+                                    type="checkbox"
+                                    className="toggle"
+                                    name="connection-description"
+                                    aria-label="connection-description"
+                                    onClick={toggleConnectionDesc}
+                                />
+                            </label>
+                            <label className="label cursor-pointer">
+                                <span className="label label-text">Node Descriptions</span>
+                                <input
+                                    type="checkbox"
+                                    className="toggle"
+                                    aria-label="node-description"
+                                    onClick={toggleNodeDesc}
+                                />
+                            </label>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
