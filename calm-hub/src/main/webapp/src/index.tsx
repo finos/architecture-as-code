b353003810fd8d98e96c7044466eb1f9e3beb152
Aidan McPhelim <aidan.mcphelim@morganstanley.com>
2025-01-09 14:55:45 +0000

    Vite migration (#747) (#775)
    * untrack META-INF
    
    * add META-INF to .gitignore
    
    * migrate to vite, addresses #747

--- calm-hub/src/main/webapp/src/index.tsx
+++ calm-hub/src/main/webapp/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '@patternfly/patternfly/patternfly.css';
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
