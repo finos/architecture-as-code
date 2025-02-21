64462c777c92cea04f3efbc2e9a355670b955d85
oliviajanejohns <olivia.johnson@morganstanley.com>
2025-02-21 11:52:34 +0000

    #778: Align CALM Hub and CALM Visualizer

--- calm-hub/src/main/webapp/src/visualizer/tests/Drawer.test.tsx
+++ calm-hub/src/main/webapp/src/visualizer/tests/Drawer.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Drawer from '../components/drawer/Drawer.js';
 
 
+
 describe('Drawer', () => {
     it('should render Drawer', () => {
         render(
