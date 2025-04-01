00ce76a35b8b1914a315f599594bab825a90c768
Adwoa-Konadu-Appiah <konadu.appiah@turntabl.io>
2025-04-01 09:57:50 +0000

    Visualize Calm Instance Without Uploading File (#1020)
    * WIP: Visualize Calm Instance Without Uploading File
    
    * WIP: Visualize Calm Instance from  Uploading File and Calm Hub
    
    * #989 correct removed file
    
    * #989 added data as a dependecy to the use affect
    
    ---------
    
    Co-authored-by: jpgough-ms <152306432+jpgough-ms@users.noreply.github.com>
    Co-authored-by: Olivia Johnson <35664512+oliviajanejohns@users.noreply.github.com>

--- calm-hub-ui/src/hub/components/json-view.tsx
+++ calm-hub-ui/src/hub/components/json-view.tsx
@@ -1,17 +1,31 @@
+import { useNavigate } from 'react-router-dom';
 import { allExpanded, defaultStyles, JsonView } from 'react-json-view-lite';
 import 'react-json-view-lite/dist/index.css';
+import { Data } from '../../model/calm.js';
 
 interface JsonRendererProps {
-    jsonString: string | undefined;
+    jsonString: Data | undefined;
 }
 
 export function JsonRenderer({ jsonString }: JsonRendererProps) {
     const defaultMessage = <div className=" text-center">Please select a document to load.</div>;
+    const navigate = useNavigate();
     const jsonView = (
-        <JsonView data={jsonString || ''} shouldExpandNode={allExpanded} style={defaultStyles} />
+        <div>
+            <button className="bg-primary hover:bg-blue-500 text-white font-bold py-2 px-4 rounded float-right" onClick={handleClick}>
+                Visualize
+            </button>
+            <JsonView data={jsonString || ''} shouldExpandNode={allExpanded} style={defaultStyles} />
+        </div>
+        
     );
+    function handleClick() {
+        navigate('/visualizer', { state: jsonString });
+      }
 
     const content = jsonString ? jsonView : defaultMessage;
 
-    return <div className="p-5 flex-1 overflow-auto bg-[#eee]">{content}</div>;
+    return (
+        <div className="p-5 flex-1 overflow-auto bg-[#eee]">{content}</div>
+    );
 }
