68a38c4e3fc60dbc7e59ab16e80c25e8484bad2d
LeighF <leigh_finegold@hotmail.co.uk>
2025-04-01 01:19:17 +0100

    Make Calm Generate generic and adaptable to future cahnges, insantiate consts, put placeholders in for required fields and support nested collections (#1089) (#444)

--- shared/src/commands/generate/components/property.ts
+++ shared/src/commands/generate/components/property.ts
@@ -1,3 +1,15 @@
+export interface JsonSchema {
+    const?: string | object,
+    enum?: string[],
+    type?: 'string' | 'integer' | 'number' | 'array' | 'boolean' | 'object',
+    $ref?: string
+    properties?: Record<string, JsonSchema>;
+    required?: string[];
+    prefixItems?: JsonSchema[];
+    items?: JsonSchema;
+    patternProperties?: Record<string, JsonSchema>;
+}
+
 export function getStringPlaceholder(name: string): string {
     return '{{ ' + name.toUpperCase().replaceAll('-', '_') + ' }}';
 }
@@ -10,23 +22,16 @@ export function getBooleanPlaceholder(name: string): string {
     return '{{ BOOLEAN_' + name.toUpperCase().replaceAll('-', '_') + ' }}';
 }
 
-interface Detail {
-    const?: string | object,
-    enum?: string[],
-    type?: 'string' | 'integer' | 'number' | 'array' | 'boolean',
-    $ref?: string
-}
-
 /**
  * Simply return the value of the const object when instantiating a const.
  * @param detail The detail from the object to instantiate
  * @returns Either the value or the object described by the 'const' property.
  */
-export function getConstValue(detail: Detail) : string | object {
+export function getConstValue(detail: JsonSchema) : string | object {
     return detail.const;
 }
 
-export function getPropertyValue(keyName: string, detail: Detail): string | string[] | number | object {
+export function getPropertyValue(keyName: string, detail: JsonSchema): string | string[] | number | object {
     // if both const and type are defined, prefer const
     if (detail.const) {
         return detail.const;
