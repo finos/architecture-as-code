cd1ef9982be4de59b7cdd66843857094491a12a2
Jim Gough <james.gough@morganstanley.com>
2025-04-03 16:22:22 +0100

    Work towards QCon final demo structure

---
+++ calm/workshop/insecure-example/cluster/cluster_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+KUBERNETES_VERSION=1.30.0
+
+echo "Starting Minikube..."
+minikube start --kubernetes-version=$KUBERNETES_VERSION
