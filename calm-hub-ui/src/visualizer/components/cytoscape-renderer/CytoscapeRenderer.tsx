0b18586ab70c1439f6c9d9e4317099d0878c014e
aamanrebello <aamanrebello@gmail.com>
2025-04-04 21:00:49 +0100

    Expand nodes when descriptions turned on

--- calm-hub-ui/src/visualizer/components/cytoscape-renderer/CytoscapeRenderer.tsx
+++ calm-hub-ui/src/visualizer/components/cytoscape-renderer/CytoscapeRenderer.tsx
@@ -6,11 +6,11 @@ import expandCollapse from 'cytoscape-expand-collapse';
 import Sidebar from '../sidebar/Sidebar.js';
 import { ZoomContext } from '../zoom-context.provider.js';
 
-// Initialize Cytoscape plugins
+//Make some information available on tooltip hover
+
 nodeEdgeHtmlLabel(cytoscape);
 expandCollapse(cytoscape);
 
-// Layout configuration
 const breadthFirstLayout = {
     name: 'breadthfirst',
     fit: true,
@@ -22,7 +22,6 @@ const breadthFirstLayout = {
     spacingFactor: 1.25,
 };
 
-// Types for nodes and edges
 export type Node = {
     classes?: string;
     data: {
@@ -65,111 +64,105 @@ const CytoscapeRenderer = ({
     const [selectedNode, setSelectedNode] = useState<Node['data'] | null>(null);
     const [selectedEdge, setSelectedEdge] = useState<Edge['data'] | null>(null);
 
-    // Generate node label templates
-    const getNodeLabelTemplateGenerator =
-        (selected = false) =>
-        (data: Node['data']) => `
-        <div class="node element ${selected ? 'selected-node' : ''}">
-            <p class="title">${data.label}</p>
-            <p class="type">${data.type}</p>
-            <p class="description">${isNodeDescActive ? data.description : ''}</p>
-        </div>
-    `;
+    function getNodeLabelTemplateGenerator(selected = false): (data: Node['data']) => string {
+        return (data: Node['data']) => {
+            return `<div class="node ${isNodeDescActive ? 'expanded' : ''} element ${selected ? 'selected-node' : ''}">
+                        <p class="title">${data.label}</p>
+                        <p class="type">${data.type}</p>
+                        <p class="description">${isNodeDescActive ? data.description : ''}</p>
+                    </div>`;
+        };
+    }
 
-    // Initialize Cytoscape instance
     useEffect(() => {
+        if (cy) {
+            //Ensure cytoscape zoom and context state are synchronised
+            if (cy.zoom() !== zoomLevel) {
+                updateZoom(cy.zoom());
+            }
+            /* eslint-disable @typescript-eslint/no-explicit-any */
+            (cy as Core & { nodeHtmlLabel: any }).nodeHtmlLabel([
+                {
+                    query: '.node',
+                    tpl: getNodeLabelTemplateGenerator(false),
+                },
+                {
+                    query: '.node:selected',
+                    tpl: getNodeLabelTemplateGenerator(true),
+                },
+            ]);
+
+            //@ts-expect-error types are missing from the library
+            cy.on('tap', 'node', (e: Event) => {
+                e.preventDefault();
+                const node = e.target as unknown as NodeSingular | null;
+                setSelectedEdge(null);
+                setSelectedNode(node?.data()); // Update state with the clicked node's data
+            });
+
+            //@ts-expect-error types are missing from the library
+            cy.on('tap', 'edge', (e: Event) => {
+                e.preventDefault();
+                const edge = e.target as unknown as EdgeSingular | null;
+                setSelectedNode(null);
+                setSelectedEdge(edge?.data()); // Update state with the clicked node's data
+            });
+
+            cy.on('zoom', () => updateZoom(cy.zoom()));
+        }
+    }, [cy, zoomLevel, updateZoom, isNodeDescActive]);
+
+    useEffect(() => {
+        // Initialize Cytoscape instance
         const container = cyRef.current;
+
         if (!container) return;
 
-        // Preserve zoom and pan state if Cytoscape instance already exists
-        const currentZoom = cy?.zoom() || 1;
-        const currentPan = cy?.pan() || { x: 0, y: 0 };
-
-        // Initialize Cytoscape
-        const updatedCy = cytoscape({
-            container,
-            elements: [...nodes, ...edges],
-            style: [
-                {
-                    selector: 'edge',
-                    style: {
-                        width: 2,
-                        'curve-style': 'bezier',
-                        label: isConDescActive ? 'data(label)' : '',
-                        'target-arrow-shape': 'triangle',
-                        'text-wrap': 'ellipsis',
-                        'text-background-color': 'white',
-                        'text-background-opacity': 1,
-                        'text-background-padding': '5px',
+        setCy(
+            cytoscape({
+                container: container, // container to render
+                elements: [...nodes, ...edges], // graph data
+                style: [
+                    {
+                        selector: 'edge',
+                        style: {
+                            width: 2,
+                            'curve-style': 'bezier',
+                            label: isConDescActive ? 'data(label)' : '', // labels from data property
+                            'target-arrow-shape': 'triangle',
+                            'text-wrap': 'ellipsis',
+                            'text-background-color': 'white',
+                            'text-background-opacity': 1,
+                            'text-background-padding': '5px',
+                        },
                     },
-                },
-                {
-                    selector: 'node',
-                    style: {
-                        width: '200px',
-                        height: '100px',
-                        shape: 'rectangle',
+                    {
+                        selector: 'node',
+                        style: {
+                            width: '200px',
+                            height: isNodeDescActive ? '200px' : '100px',
+                            shape: 'rectangle',
+                        },
                     },
-                },
-                {
-                    selector: ':parent',
-                    style: {
-                        label: 'data(label)',
+                    {
+                        selector: ':parent',
+                        style: {
+                            label: 'data(label)',
+                        },
                     },
-                },
-            ],
-            layout: breadthFirstLayout,
-            boxSelectionEnabled: true,
-            minZoom: 0.1,
-            maxZoom: 5,
-        });
+                ],
+                layout: breadthFirstLayout,
+                boxSelectionEnabled: true,
+            })
+        );
+    }, [nodes, edges, isConDescActive]); // Re-render on cy, nodes or edges change
 
-        // Restore zoom and pan state
-        updatedCy.zoom(currentZoom);
-        updatedCy.pan(currentPan);
-
-        // Add event listeners
-        updatedCy.on('tap', 'node', (e) => {
-            const node = e.target as NodeSingular;
-            setSelectedEdge(null);
-            setSelectedNode(node?.data());
-        });
-
-        updatedCy.on('tap', 'edge', (e) => {
-            const edge = e.target as EdgeSingular;
-            setSelectedNode(null);
-            setSelectedEdge(edge?.data());
-        });
-
-        updatedCy.on('zoom', () => updateZoom(updatedCy.zoom()));
-
-        // Update node labels dynamically
-        /* eslint-disable @typescript-eslint/no-explicit-any */
-        (updatedCy as Core & { nodeHtmlLabel: any }).nodeHtmlLabel([
-            {
-                query: '.node',
-                tpl: getNodeLabelTemplateGenerator(false),
-            },
-            {
-                query: '.node:selected',
-                tpl: getNodeLabelTemplateGenerator(true),
-            },
-        ]);
-
-        // Set Cytoscape instance
-        setCy(updatedCy);
-
-        return () => {
-            updatedCy.destroy(); // Clean up Cytoscape instance
-        };
-    }, [nodes, edges, isConDescActive, isNodeDescActive, updateZoom]);
-
-    // Synchronize zoom level with context
     useEffect(() => {
-        if (cy && cy.zoom() !== zoomLevel) {
-            cy.zoom(zoomLevel);
+        //Ensure cytoscape zoom and context state are synchronised
+        if (cy?.zoom() !== zoomLevel) {
+            cy?.zoom(zoomLevel);
         }
-    }, [cy, zoomLevel]);
+    }, [zoomLevel]);
 
     return (
         <div className="relative flex m-auto border">
@@ -179,12 +172,29 @@ const CytoscapeRenderer = ({
                     <span className="text-m font-semibold">{title}</span>
                 </div>
             )}
-            <div ref={cyRef} className="flex-1 bg-white visualizer" style={{ height: '100vh' }} />
+            <div
+                ref={cyRef}
+                className="flex-1 bg-white visualizer"
+                style={{
+                    height: '100vh',
+                }}
+            />
             {selectedNode && (
-                <Sidebar selectedData={selectedNode} closeSidebar={() => setSelectedNode(null)} />
+                <div className="absolute right-0 h-full">
+                    <Sidebar
+                        selectedData={selectedNode}
+                        closeSidebar={() => setSelectedNode(null)}
+                    />
+                </div>
             )}
+
             {selectedEdge && (
-                <Sidebar selectedData={selectedEdge} closeSidebar={() => setSelectedEdge(null)} />
+                <div className="absolute right-0 h-full">
+                    <Sidebar
+                        selectedData={selectedEdge}
+                        closeSidebar={() => setSelectedEdge(null)}
+                    />
+                </div>
             )}
         </div>
     );
