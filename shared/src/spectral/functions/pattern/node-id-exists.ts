b1384937d7ecb46cb515928fc80c685f6bc1d60b
Aidan McPhelim <aidan.mcphelim@morganstanley.com>
2025-04-04 12:48:50 +0100

    working on validate command

--- shared/src/spectral/functions/pattern/node-id-exists.ts
+++ shared/src/spectral/functions/pattern/node-id-exists.ts
@@ -8,11 +8,13 @@ export default (input, _, context) => {
     }
 
     const names = JSONPath({path: '$.properties.nodes.prefixItems[*].properties.unique-id.const', json: context.document.data});
+    const oneofs = JSONPath({path: '$.properties.nodes.prefixItems[*].oneOf[*].properties.unique-id.const', json: context.document.data});
+    const anyofs = JSONPath({path: '$.properties.nodes.prefixItems[*].anyOf[*].properties.unique-id.const', json: context.document.data});
 
     // get uniqueIds of all nodes
     const results = [];
 
-    if (!names.includes(input)) {
+    if (!names.includes(input) && !oneofs.includes(input) && !anyofs.includes(input)) {
         results.push({
             message: `'${input}' does not refer to the unique-id of an existing node.`,
             path: [...context.path]
