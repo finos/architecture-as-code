5d8a0f69968f01b80a7fc0125d2e0017235f3327
LeighF <leigh_finegold@hotmail.co.uk>
2025-06-23 21:17:38 +0100

    Fixing docify so that it works for node 22.12.0 and >=23 (#1386)

--- shared/test_fixtures/docify/workshop/expected-output/secure/src/remark/remark-replace-links.js
+++ shared/test_fixtures/docify/workshop/expected-output/secure/src/remark/remark-replace-links.js
@@ -1,13 +1,12 @@
 import { visit } from 'unist-util-visit';
-/* eslint-disable no-undef */
-module.exports = function remarkReplaceLinks() {
+
+export default function remarkReplaceLinks() {
     return (tree) => {
         visit(tree, 'link', (node) => {
-            if (node.url && node.url.startsWith('https://calm.finos.org/traderx/control-requirements/')) {
+            if (node.url ) {
                 const lastSegment = node.url.split('/').pop();
                 node.url = `/control-requirements/${lastSegment}`;
 
-                // Ensure node.properties exists before setting attributes
                 node.data = node.data || {};
                 node.data.hProperties = node.data.hProperties || {};
                 node.data.hProperties.target = '_blank';
